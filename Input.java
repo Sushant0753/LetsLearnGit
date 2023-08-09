1e5475aae96f574e47ef8ca9c6e4a3cc77a80882
Sushant Kumar <mail.sushantthakur@gmail.com>
2023-08-09 17:16:10 +0530

    initial commit

---
+++ Input.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Input{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int a = sc.nextInt();
+        System.out.println("The entered number is: "+ a);
+    }
+}
